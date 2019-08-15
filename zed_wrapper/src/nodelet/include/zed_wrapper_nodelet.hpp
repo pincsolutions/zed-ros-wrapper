b08dcfbc019dfb5e7eeed5d795aca6109daf3041
ShawnRicardo-PINC <sricardo@pinc.com>
2019-08-15 13:38:53 -0700

    fixed syntax bug

--- zed_wrapper/src/nodelet/include/zed_wrapper_nodelet.hpp
+++ zed_wrapper/src/nodelet/include/zed_wrapper_nodelet.hpp
@@ -542,7 +542,7 @@ namespace zed_wrapper {
 
         // Point cloud variables
         sl::Mat mCloud;
-        std::vector<std::pair<ros::Time, sl::Mat> clouds;
+        std::vector<std::pair<ros::Time, sl::Mat>> clouds;
 
         sensor_msgs::PointCloud2Ptr mPointcloudMsg;
 #if ((ZED_SDK_MAJOR_VERSION>2) || (ZED_SDK_MAJOR_VERSION==2 && ZED_SDK_MINOR_VERSION>=8) )
