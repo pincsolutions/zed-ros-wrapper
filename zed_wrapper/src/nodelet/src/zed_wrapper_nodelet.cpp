af7e5c7b2b33dad902e8206d7f32a8b3b2a03a93
ShawnRicardo-PINC <sricardo@pinc.com>
2019-08-21 09:12:40 -0700

    including check on size before owrking on vector

--- zed_wrapper/src/nodelet/src/zed_wrapper_nodelet.cpp
+++ zed_wrapper/src/nodelet/src/zed_wrapper_nodelet.cpp
@@ -1631,8 +1631,6 @@ namespace zed_wrapper {
         int ind = 0;
         bool cloudFound = false;
 
-        std::cout << "a" << std::endl;
-
         for (; ind < clouds.size(); ind++)
         {
             if ((message->imageStamp.data.sec == clouds[ind].first.sec) && (message->imageStamp.data.nsec == clouds[ind].first.nsec))
@@ -1642,8 +1640,6 @@ namespace zed_wrapper {
             }
         }
 
-        std::cout << "b" << std::endl;
-
         if (cloudFound)
         {
             sl::float4 point3d;
@@ -1651,8 +1647,6 @@ namespace zed_wrapper {
             std::vector<double> y_pts;
             std::vector<double> z_pts;
 
-            std::cout << "c" << std::endl;
-            
             for (int i = 1; i < message->poses.size(); i++)
             {
                 clouds[ind].second.getValue(size_t(int(message->poses[i].position.y)), size_t(int(message->poses[i].position.x)), &point3d);
@@ -1664,37 +1658,32 @@ namespace zed_wrapper {
                     z_pts.push_back(point3d.z);
                 }
             }
-            std::cout << "d" << std::endl;
 
-            std::vector<double> x_mvd = mean_var_dev(x_pts);
-            std::vector<double> y_mvd = mean_var_dev(y_pts);
-            std::vector<double> z_mvd = mean_var_dev(z_pts);
+            if ((x_pts.size() > 0) && (y_pts.size() > 0) && (z_pts.size() > 0))
+            {
+                std::vector<double> x_mvd = mean_var_dev(x_pts);
+                std::vector<double> y_mvd = mean_var_dev(y_pts);
+                std::vector<double> z_mvd = mean_var_dev(z_pts);
 
-            std::cout << "e" << std::endl;
-
-            // construct ros message
-            std_msgs::Float64MultiArray output_msg;
-            output_msg.data.push_back(message->poses[0].position.x);
-            output_msg.data.push_back(x_mvd[0]);
-            output_msg.data.push_back(x_mvd[1]);
-            output_msg.data.push_back(x_mvd[2]);
-            output_msg.data.push_back(y_mvd[0]);
-            output_msg.data.push_back(y_mvd[1]);
-            output_msg.data.push_back(y_mvd[2]);
-            output_msg.data.push_back(z_mvd[0]);
-            output_msg.data.push_back(z_mvd[1]);
-            output_msg.data.push_back(z_mvd[2]);
-            output_msg.data.push_back(mPointCloudTime.sec);
-            output_msg.data.push_back(mPointCloudTime.nsec);
-            
-            std::cout << "o" << std::endl;
-            
-            output_msg.data.push_back(message->front_side_active);
-        
-            std::cout << "f" << std::endl;
-
-            // publish
-            mPubXPixelToPcLoc.publish(output_msg);
+                // construct ros message
+                std_msgs::Float64MultiArray output_msg;
+                output_msg.data.push_back(message->poses[0].position.x);
+                output_msg.data.push_back(x_mvd[0]);
+                output_msg.data.push_back(x_mvd[1]);
+                output_msg.data.push_back(x_mvd[2]);
+                output_msg.data.push_back(y_mvd[0]);
+                output_msg.data.push_back(y_mvd[1]);
+                output_msg.data.push_back(y_mvd[2]);
+                output_msg.data.push_back(z_mvd[0]);
+                output_msg.data.push_back(z_mvd[1]);
+                output_msg.data.push_back(z_mvd[2]);
+                output_msg.data.push_back(mPointCloudTime.sec);
+                output_msg.data.push_back(mPointCloudTime.nsec);
+                output_msg.data.push_back(message->front_side_active);
+                
+                // publish
+                mPubXPixelToPcLoc.publish(output_msg);
+            }
         }
     }
 
